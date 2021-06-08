a2d768aa9296e0517d50b17059a60535cefc80df
Andrei Dzebysh <presnia@yahoo.com>
2021-06-08 18:11:14 +0300

    Added functionality and adjusted styles

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=60e5fe697a276cb0e42aec5d14c9ec8c&query="';
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -11,10 +12,12 @@ const search = document.getElementById('search');
 const getMovies = async (url) => {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.results);
+
+  showMovies(data.results);
 };
 getMovies(API_URL);
 
+// Getting movies by keyword in search
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
@@ -27,3 +30,40 @@ form.addEventListener('submit', (e) => {
     window.location.reload();
   }
 });
+
+// Classifying rates
+const getClassByRate = (vote) => {
+  if (vote >= 8) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+};
+
+// Putting movies into DOM
+function showMovies(movies) {
+  main.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement('section');
+    movieEl.classList.add('movie');
+
+    movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}">
+        <section class="movie-info">
+          <h3>${title}</h3>
+          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </section>
+        <section class="overview">
+          <h3>Overview</h3>
+          ${overview}
+        </section>
+    `;
+
+    main.append(movieEl);
+  });
+}
